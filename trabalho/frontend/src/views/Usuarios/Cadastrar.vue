54d8e32134e24d2071ead400ade1ceae7181928a
Igor Emanuel Carvalho da Cruz <igor.cruz4@ufla.br>
2020-12-17 11:03:18 -0300

    login nao repetido

--- trabalho/frontend/src/views/Usuarios/Cadastrar.vue
+++ trabalho/frontend/src/views/Usuarios/Cadastrar.vue
@@ -261,29 +261,19 @@
         methods: {
 
             submit () {
+                
 
-                if(!UsuarioServices.listar()
+                UsuarioServices.cadastrar(this.usuario)
                     .then(resposta => {
-                        //console.log(resposta.data)
-                        resposta.data.some(element => {
-                            console.log(element)
-                            if (this.usuario.login == element.login) {
-                               this.$toast.error("Informe um login de usuario válido!")
-                               return true;
-                            }
-                        });
-                    })){
-                        UsuarioServices.cadastrar(this.usuario)
-                            .then(resposta => {
-                                console.log(resposta.data)
-                                this.$toast.success('Usuario salvo com sucesso!')
-                                window.location.href = this.voltar
-                            })
-                            .catch(error => {
-                                console.log(error)
-                                this.$toast.error("Erro ao cadastrar usuario")
-                            })
-                    }
+                        console.log(resposta.data)
+                        this.$toast.success('Usuario salvo com sucesso!')
+                        window.location.href = this.voltar
+                    })
+                    .catch(error => {
+                        console.log(error)
+                        this.$toast.error("Erro ao cadastrar usuario")
+                    })
+
             },
 
             attEndereco(){
